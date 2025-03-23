4164f498874d325d8c23fe537503444e25c36436
Huy1874 <163031019+Huy1874@users.noreply.github.com>
2025-03-23 23:41:21 +0700

    add setting

--- lib/pages/Setting/Setting/setting_screen.dart
+++ lib/pages/Setting/Setting/setting_screen.dart
@@ -22,46 +22,15 @@ class SettingsScreen extends StatelessWidget {
                   SizedBox(height: 30.h),
                   _buildProfileInfoSection(context),
                   SizedBox(height: 30.h),
-                  _buildAddressSection(context),
-                  SizedBox(height: 8.h),
+                  _buttonAddress(context),
+                  SizedBox(height: 10.h),
                   _buildSupportChatSection(context),
-                  SizedBox(height: 8.h),
-                  CustomElevatedButton(
-                    height: 54.h,
-                    text: "Đổi mật khẩu",
-                    margin: EdgeInsets.symmetric(horizontal: 24.h),
-                    leftIcon: Container(
-                      margin: EdgeInsets.only(right: 10.h),
-                      child: CustomImageView(
-                        imagePath: ImageConstant.imgTeenyiconspasswordoutline,
-                        height: 22.h,
-                        width: 22.h,
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    buttonStyle: CustomButtonStyles.fillWhiteA,
-                    buttonTextStyle:
-                        CustomTextStyles.titleMediumGabaritoGray900SemiBold,
-                  ),
-                  SizedBox(height: 8.h),
-                  CustomElevatedButton(
-                    height: 50.h,
-                    text: "Đăng xuất",
-                    margin: EdgeInsets.symmetric(horizontal: 24.h),
-                    leftIcon: Container(
-                      margin: EdgeInsets.only(right: 10.h),
-                      child: CustomImageView(
-                        imagePath: ImageConstant.imgThumbsup,
-                        height: 24.h,
-                        width: 24.h,
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    buttonStyle: CustomButtonStyles.fillWhiteA,
-                    buttonTextStyle:
-                        CustomTextStyles.titleMediumGabaritoRed500SemiBold,
-                  ),
-                  SizedBox(height: 58.h),
+                  SizedBox(height: 10.h),
+                  _buttonChangePassword(context),
+                  SizedBox(height: 10.h),
+                  _buttonLogout(context),
+                  SizedBox(height: 10.h),
+                  
                 ],
               ),
             ),
@@ -140,26 +109,26 @@ class SettingsScreen extends StatelessWidget {
                         decoration: AppDecoration.row35,
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
                             CustomImageView(
-                              imagePath: ImageConstant.imgEllipse14,
+                              imagePath: ImageConstant.imgProfile,
                               height: 90.h,
                               width: 90.h,
                               radius: BorderRadius.circular(44.h),
                               margin: EdgeInsets.only(top: 94.h),
                             ),
+                            Container(
+                              margin: EdgeInsets.only(top: 94.h, left: 10),
+                              child: Text(
+                                "Gilbert Jones",
+                                style: CustomTextStyles
+                                    .bodyLargeBalooBhaijaanDeeppurple50,
+                              ),
+                            ),
                           ],
                         ),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 30.h),
-                    child: Text(
-                      "Gilbert Jones",
-                      style:
-                          CustomTextStyles.bodyLargeBalooBhaijaanDeeppurple50,
                     ),
                   ),
                 ],
@@ -215,82 +184,91 @@ class SettingsScreen extends StatelessWidget {
     );
   }
 
-  /// Section Widget
-  Widget _buildAddressSection(BuildContext context) {
-    return Container(
+  Widget _buttonAddress(BuildContext context) {
+    return CustomElevatedButton(
+      alignment: Alignment.centerLeft,
+      height: 50.h,
+      text: "Địa chỉ",
       margin: EdgeInsets.symmetric(horizontal: 24.h),
-      padding: EdgeInsets.symmetric(
-        horizontal: 12.h,
-        vertical: 14.h,
+      leftIcon: Container(
+        margin: EdgeInsets.only(right: 10.h),
+        child: CustomImageView(
+          imagePath: ImageConstant.imgFa6SolidMapLocation,
+          height: 24.h,
+          width: 24.h,
+          fit: BoxFit.contain,
+        ),
       ),
-      decoration: AppDecoration.fillWhiteA.copyWith(
-        borderRadius: BorderRadiusStyle.roundedBorder8,
+      rightIcon: CustomImageView(
+        imagePath: ImageConstant.imgIconsaxBrokenArrowright2,
+        height: 24.h,
+        width: 26.h,
       ),
-      width: double.maxFinite,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomImageView(
-            imagePath: ImageConstant.imgFa6SolidMapLocation,
-            height: 24.h,
-            width: 28.h,
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 10.h),
-            child: Text(
-              "Địa chỉ",
-              style: CustomTextStyles.titleMediumGabaritoGray900SemiBold,
-            ),
-          ),
-          Spacer(),
-          CustomImageView(
-            imagePath: ImageConstant.imgIconsaxBrokenArrowleft2,
-            height: 24.h,
-            width: 26.h,
-          ),
-        ],
+      buttonStyle: CustomButtonStyles.fillWhiteA,
+      buttonTextStyle: CustomTextStyles.titleMediumGabaritoGray900SemiBold,
+    );
+  }
+
+  Widget _buttonChangePassword(BuildContext context) {
+    return CustomElevatedButton(
+      height: 54.h,
+      text: "Đổi mật khẩu",
+      margin: EdgeInsets.symmetric(horizontal: 24.h),
+      leftIcon: Container(
+        margin: EdgeInsets.only(right: 10.h),
+        child: CustomImageView(
+          imagePath: ImageConstant.imgTeenyiconspasswordoutline,
+          height: 22.h,
+          width: 22.h,
+          fit: BoxFit.contain,
+        ),
       ),
+      buttonStyle: CustomButtonStyles.fillWhiteA,
+      buttonTextStyle: CustomTextStyles.titleMediumGabaritoGray900SemiBold,
     );
   }
 
   /// Section Widget
   Widget _buildSupportChatSection(BuildContext context) {
-    return Container(
+    return CustomElevatedButton(
+      height: 54.h,
+      text: "Trò chuyện với trung tâm hỗ trợ",
       margin: EdgeInsets.symmetric(horizontal: 24.h),
-      padding: EdgeInsets.symmetric(
-        horizontal: 12.h,
-        vertical: 16.h,
+      leftIcon: Container(
+        margin: EdgeInsets.only(right: 10.h),
+        child: CustomImageView(
+          imagePath: ImageConstant.imgBxSupport,
+          height: 22.h,
+          width: 22.h,
+          fit: BoxFit.contain,
+        ),
       ),
-      decoration: AppDecoration.fillWhiteA.copyWith(
-        borderRadius: BorderRadiusStyle.roundedBorder8,
+      rightIcon: CustomImageView(
+        imagePath: ImageConstant.imgIconsaxBrokenArrowright2,
+        height: 24.h,
+        width: 26.h,
       ),
-      width: double.maxFinite,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomImageView(
-            imagePath: ImageConstant.imgBxSupport,
-            height: 24.h,
-            width: 26.h,
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 10.h),
-            child: Text(
-              "Trò chuyện với trung tâm hỗ trợ",
-              style: CustomTextStyles.titleMediumGabaritoGray900SemiBold,
-            ),
-          ),
-          Spacer(),
-          CustomImageView(
-            imagePath: ImageConstant.imgIconsaxBrokenArrowleft2,
-            height: 24.h,
-            width: 26.h,
-          ),
-        ],
-      ),
+      buttonStyle: CustomButtonStyles.fillWhiteA,
+      buttonTextStyle: CustomTextStyles.titleMediumGabaritoGray900SemiBold,
     );
   }
 
-
-
+  Widget _buttonLogout(BuildContext context) {
+    return CustomElevatedButton(
+      height: 50.h,
+      text: "Đăng xuất",
+      margin: EdgeInsets.symmetric(horizontal: 24.h),
+      leftIcon: Container(
+        margin: EdgeInsets.only(right: 10.h),
+        child: CustomImageView(
+          imagePath: ImageConstant.imgThumbsup,
+          height: 24.h,
+          width: 24.h,
+          fit: BoxFit.contain,
+        ),
+      ),
+      buttonStyle: CustomButtonStyles.fillWhiteA,
+      buttonTextStyle: CustomTextStyles.titleMediumGabaritoRed500SemiBold,
+    );
+  }
 }
