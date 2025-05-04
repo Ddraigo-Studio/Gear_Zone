b7a10bef22431eddb4abcbe3d28a8d1039b64579
Văn ThịThanh Hương <52200128@student.tdtu.edu.vn>
2025-05-04 22:09:11 +0700

    update user

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,21 +1,18 @@
 class UserModel {
-  String uid; // ID người dùng (do Firebase tạo)
-  String email; // Email của người dùng
-  String name; // Tên của người dùng
+  String uid; 
+  String email; 
+  String name;
 
-  // Constructor để tạo UserModel
   UserModel({required this.uid, required this.email, required this.name});
 
-  // Chuyển dữ liệu từ Firestore (Map) thành đối tượng UserModel
   factory UserModel.fromMap(Map<String, dynamic> data) {
     return UserModel(
-      uid: data["uid"] ?? "",  // Lấy giá trị từ Map, nếu không có thì để ""
+      uid: data["uid"] ?? "",  
       email: data["email"] ?? "",
-      name: data["name"] ?? "Không có tên", // Nếu không có name thì đặt mặc định
+      name: data["name"] ?? "Không có tên", 
     );
   }
 
-  // Chuyển đối tượng UserModel thành Map để lưu vào Firestore
   Map<String, dynamic> toMap() {
     return {
       "uid": uid,
